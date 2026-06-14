e0c1a621ebec81a89c6e09a967b8fbda20080fb0
infobaik <infobaikmenarik@gmail.com>
2026-06-14 18:34:33 +0700

    Create index.ts

---
+++ src/services/providers/index.ts
@@ -0,0 +1,20 @@
+import { processDigiflazzOrder } from './digiflazz'
+import { processApigamesOrder } from './apigames'
+
+// Fungsi routing dinamis
+export async function dispatchProviderOrder(
+  providerName: string, 
+  providerCredentials: any, 
+  skuCode: string, 
+  customerNumber: string, 
+  trxId: string
+) {
+  switch (providerName.toLowerCase()) {
+    case 'digiflazz':
+      return await processDigiflazzOrder(providerCredentials, skuCode, customerNumber, trxId)
+    case 'apigames':
+      return await processApigamesOrder(providerCredentials, skuCode, customerNumber, trxId)
+    default:
+      throw new Error(`Provider module '${providerName}' belum diimplementasikan.`)
+  }
+}
