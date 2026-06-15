04ec59b0eabc95aeefccea4e83bbfd6c8d5470bf
infobaik <infobaikmenarik@gmail.com>
2026-06-15 21:19:50 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,29 @@ import publicApi from './api/public/v1'
 import adminApi from './api/admin/v1'
 import waApi from './api/wa/v1'
 import tgApi from './api/tg/v1'
-import digiflazzWebhook from './api/webhook/digiflazz'
+
+// 1. Import modul Supplier (Untuk Sinkronisasi Cronjob)
+import supplierApi from './api/supplier/index' 
+
+// 2. Import modul Webhook (Untuk Menerima Callback dari Digiflazz)
+import digiflazzWebhook from './api/webhook/digiflazz' 
+
 const api = new Hono()
 
 // Mount rute public (Tanpa proteksi token)
 api.route('/api/public/v1', publicApi)
 
 // Mount rute khusus klien/bot
-// Catatan: Di dalam filenya nanti akan ada middleware pengecekan API Key
 api.route('/api/wa/v1', waApi)
 api.route('/api/tg/v1', tgApi)
 
-// Mount rute khusus admin (Bisa ditambahkan middleware auth API di sini jika via eksternal)
+// Mount rute khusus admin
 api.route('/api/admin/v1', adminApi)
+
+// Mount rute API Sinkronisasi Supplier
+api.route('/api/supplier/v1', supplierApi)
+
+// Mount rute Webhook Digiflazz
 api.route('/api/webhook/digiflazz', digiflazzWebhook)
+
 export default api
