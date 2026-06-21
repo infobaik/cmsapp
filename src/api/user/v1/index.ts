1f976e417e309e95fbecf658e0d715edb950afec
infobaik <infobaikmenarik@gmail.com>
2026-06-21 14:05:50 +0700

    Update index.ts

--- src/api/user/v1/index.ts
+++ src/api/user/v1/index.ts
@@ -53,7 +53,7 @@ app.post('/wallet/deposit', async (c) => {
     const body = await c.req.parseBody()
     const amount = Number(body.amount)
 
-    if (amount < 10000) return c.json({ success: false, message: 'Minimal deposit Rp 10.000' }, 400)
+    if (amount < 1000) return c.json({ success: false, message: 'Minimal deposit Rp 10.000' }, 400)
     
     const depositId = `DEP-${crypto.randomUUID().split('-')[0].toUpperCase()}`
 
