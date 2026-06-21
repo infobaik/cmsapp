1f72b70f92096bdcd149e85990731606c5e3bb10
infobaik <infobaikmenarik@gmail.com>
2026-06-21 13:23:39 +0700

    Update index.ts

--- src/api/user/v1/index.ts
+++ src/api/user/v1/index.ts
@@ -129,7 +129,7 @@ app.post('/wallet/deposit', async (c) => {
     const body = await c.req.parseBody()
     const amount = Number(body.amount)
 
-    if (amount < 10000) return c.redirect('/user/wallet?error=minimal_10000')
+    if (amount < 1000) return c.redirect('/user/wallet?error=minimal_10000')
     
     const depositId = `DEP-${crypto.randomUUID().split('-')[0].toUpperCase()}`
 
